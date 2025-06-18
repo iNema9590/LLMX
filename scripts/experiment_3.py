3194252e835a868c858f486ae217c853b5b82366
Tarik Kalai <Tarik.Kalai@ulb.be>
2025-06-18 14:32:52 +0200

    Update experiment 3 set up

--- scripts/experiment_3.py
+++ scripts/experiment_3.py
@@ -41,7 +41,7 @@ start = time.time()
 csv_path = os.path.join(current_dir, f'../data/synthetic_data/{args.dataset}.csv')
 
 df = pd.read_csv(csv_path)
-df_save_results = pd.DataFrame(columns = ["query", "context", "provided_answer", "top_k", "methods_top_k", "precision_top_k", "doc_id"])
+df_save_results = pd.DataFrame(columns = ["query", "context", "provided_answer", "top_k", "methods_top_k", "precision_top_k", "doc_id", "FM_matrix"])
 print("Data Loaded!")
 
 num_questions_to_run = df.shape[0]
@@ -150,7 +150,7 @@ for i in tqdm(range(num_questions_to_run), desc="Processing Questions", disable=
             if actual_samples > 0: 
                 results_for_query[f"ContextCite{actual_samples}"] = harness.compute_contextcite_weights(num_samples=actual_samples, sampling="uniform",  seed=SEED) #lasso_alpha=0.01,
                 results_for_query[f"KernelShap{actual_samples}"] = harness.compute_contextcite_weights(num_samples=actual_samples, sampling="kernel_shap",  seed=SEED) #lasso_alpha=0.01,
-                # results_for_query[f"WSS{actual_samples}"] = harness.compute_wss(num_samples=actual_samples,  seed=SEED, sampling="uniform") #lasso_alpha=0.01,
+                results_for_query[f"WSS{actual_samples}"], F = harness.compute_wss(num_samples=actual_samples,  seed=SEED, sampling="uniform") #lasso_alpha=0.01,
                 results_for_query[f"BetaShap (U){actual_samples}"] = harness.compute_beta_shap(num_iterations_max=T_iterations_map[size_key], beta_a=0.5, beta_b=0.5, max_unique_lookups=actual_samples, seed=SEED)
                 results_for_query[f"TMC{actual_samples}"] = harness.compute_tmc_shap(num_iterations_max=T_iterations_map[size_key], performance_tolerance=0.001, max_unique_lookups=actual_samples, seed=SEED)
         
@@ -168,12 +168,13 @@ for i in tqdm(range(num_questions_to_run), desc="Processing Questions", disable=
 
         df_save_results.loc[i, "query"] = query
         df_save_results.loc[i, "scoring"] = [results_for_query]
-        df_save_results.loc[i, "top_k"] = [exhaustive_top_2]
+        df_save_results.loc[i, "top_k"] = [[exhaustive_top_2]]
         df_save_results.loc[i, "precision_top_k"] = [precision_top_k]
         # print("Flags Value: ", flags)
         df_save_results.loc[i, "doc_id"] = [[flags]]
         df_save_results.loc[i, "context"] = [[docs]]
         df_save_results.loc[i, "provided_answer"] = harness.target_response
+        df_save_results.loc[i, "FM_matrix"] = F
 
 
         print("RESULTS FOR QUERY: ", df_save_results)
